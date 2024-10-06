8bfa5ef5684a5aabf2f26f01537cc34c7f72bf3e
AlexOfRivia <alexbhandari22@gmail.com>
2024-10-06 15:19:30 +0200

    Added game class

---
+++ Snake/Game.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <ctime>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+
+/*
+This class is used as the "Game Engine". Everything is happening here
+*/
+
+class Game
+{
+	public:
+	//public functions
+	void updateEvents();
+	void Update();
+	void Render();
+
+
+	//bool for checking if the window is runing
+	const bool isRunning() const;
+
+	//Constructor & Destructor
+	Game();
+	~Game();
+
+	private:
+	sf::RenderWindow* window;
+	sf::Event event;
+
+	//private functions
+	void InitVariables();
+	void InitWindow();
+};
+
